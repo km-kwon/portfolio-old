f13c78a343968d27269ceb33a319f3112f28ace3
km-kwon <gmkwon@suresofttech.com>
2026-01-14 15:51:51 +0900

    FEAT: footer 이름 변경

--- src/component/Footer/footer.tsx
+++ src/component/Footer/footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
 
   return (
     <footer className="max-w-[1040px] mx-auto px-5 pb-6 pt-4 border-t border-white/5 flex flex-col gap-2 text-[11px] text-fg-muted sm:flex-row sm:justify-between">
-      <span>© {currentYear} Your Name. All rights reserved.</span>
+      <span>© {currentYear} Kwon-KyoungMin All rights reserved.</span>
       <span>Built with React · TypeScript · minimal black &amp; white.</span>
     </footer>
   );
